70714aa966bb1c9e90e13d055203ac0dd22d1ce3
GiorgettaR <giorgetta.roberto@gmail.com>
2024-03-08 17:58:32 +0100

    ADD/FIX: HTML/CSS/JS - thumbnails images, style and interaction

--- js/app.js
+++ js/app.js
@@ -8,31 +8,39 @@ const imagesUrl =[
     './img/05.webp'
 ]
 
+
+// imagesUrl.length
+
 // ciclo for che compone l'html delle immagini
 
 let imagesHtmlCode = ``;
 for (i = 0; i < imagesUrl.length; i++){
     imagesHtmlCode +=
-    `<div class="file">
+    `<div class="img">
         <img src="${imagesUrl[i]}">
     </div>
     `;
 }
-console.log(imagesUrl.length);
 
 // aggiungere struttura immagini all'html
 
-const imagesBox = document.querySelector('.carousel');
+const imagesBox = document.getElementById('imageBox');
 imagesBox.innerHTML += imagesHtmlCode;
 
-// creazione HTMLCollectio (array) file immagini
+const thumbnailsBox = document.getElementById('thumbnails');
+thumbnailsBox.innerHTML += imagesHtmlCode;
 
-const imagesDiv = document.getElementsByClassName('file');
+// creazione HTMLCollections (arrays) immagini
+
+const imagesDiv = document.getElementById('imageBox').getElementsByClassName('img');
 console.log(imagesDiv);
+
+const thumbnailsDiv = document.getElementById('thumbnails').getElementsByClassName('img');
+console.log(thumbnailsDiv);
 
 // attivare la prima immagine
 
-let activeImage = document.querySelector('.file');
+let activeImage = document.querySelector('.img');
 activeImage.classList.add('active');
 let activeImageIndex = 0;
 
@@ -42,23 +50,25 @@ const arrowUp = document.querySelector('.arrowUp');
 const arrowDown = document.querySelector('.arrowDown');
 
 arrowDown.addEventListener('click', function(){
+    imagesDiv[activeImageIndex].classList.remove('active');
+    thumbnailsDiv[activeImageIndex].classList.remove('active');
     if (activeImageIndex + 1 !== imagesUrl.length){
-        imagesDiv[activeImageIndex].classList.remove('active');
         activeImageIndex++;
     } else {
-        imagesDiv[activeImageIndex].classList.remove('active');
         activeImageIndex = 0;
     }
     imagesDiv[activeImageIndex].classList.add('active');
+    thumbnailsDiv[activeImageIndex].classList.add('active');
 })
 
 arrowUp.addEventListener('click', function(){
+    imagesDiv[activeImageIndex].classList.remove('active');
+    thumbnailsDiv[activeImageIndex].classList.remove('active');
     if (activeImageIndex + 1 !== 1){
-        imagesDiv[activeImageIndex].classList.remove('active');
         activeImageIndex--;
     } else {
-        imagesDiv[activeImageIndex].classList.remove('active');
         activeImageIndex = imagesUrl.length - 1;
     }
     imagesDiv[activeImageIndex].classList.add('active');
+    thumbnailsDiv[activeImageIndex].classList.add('active');
 })
